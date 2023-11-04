799a1b4de087132be6f192c8e00d299965fad033
Remco Stoeten <remcostoeten@hotmail.com>
2023-11-04 21:26:18 +0100

    fix bugs for compile

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Toaster } from 'sonner'
-import 'styles/globals.scss'
+import '@/styles/globals.scss'
 
 const inter = Inter({ subsets: ['latin'] })
 
